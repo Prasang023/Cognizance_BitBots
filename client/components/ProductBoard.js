752c3383c70c90ef249c2c33113e56dfac873eab
Hetu Patel <hetu200211@gmail.com>
2023-03-25 16:34:20 +0530

    scan component style

--- client/components/ProductBoard.js
+++ client/components/ProductBoard.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 function ProductBoard({ scannedData }) {
-  return <div>{}</div>;
+  return (
+    <div className="product-detail-box">
+      <div className="photo"></div>
+      <div className="box">
+        <p>Name</p>
+        <div className="child">
+          <p>Title</p>
+        </div>
+      </div>
+      <div className="box">
+        <p>Activate time</p>
+        <div className="child">
+          <p>1-2-2002</p>
+        </div>
+      </div>
+      <div className="box">
+        <p>Product status</p>
+        <div className="child">
+          <p>Ma chudae</p>
+        </div>
+      </div>
+      <div className="box button">
+        <p>Sell Product</p>
+      </div>
+    </div>
+  );
 }
 
 export default ProductBoard;
