f0d2a0933b6bc5df49c4ee256f9f1b7ffd22d6a8
Prasang023 <prasang023@gmail.com>
2023-03-26 02:05:45 +0530

    logistics contract updated

--- client/components/ProductBoardGlobal.js
+++ client/components/ProductBoardGlobal.js
@@ -1,84 +1,98 @@
-import React, { useState, useEffect } from "react";
-import * as Push from "@pushprotocol/restapi";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect } from "react"
+import * as Push from "@pushprotocol/restapi"
+import { useDispatch, useSelector } from "react-redux"
+import Router from "next/router"
 // import Success from "@/components/Success";
 // import Error from "@/components/Error";
-import { useSigner } from "wagmi";
-import { setSuccess } from "@/redux/slices/success";
-import { setError } from "@/redux/slices/error";
-import InputBox from "./InputBox";
-import { getProductDetailsById, sellProduct } from "@/redux/slices/retailer";
-import Image from "next/image";
-import logo from "../assets/logo/dwar.png";
+import { useSigner } from "wagmi"
+import { setSuccess } from "@/redux/slices/success"
+import { setError } from "@/redux/slices/error"
+import InputBox from "./InputBox"
+import { getProductDetailsById, sellProduct } from "@/redux/slices/retailer"
+import Image from "next/image"
+import logo from "../assets/logo/dwar.png"
+import { resellProduct, transferNft } from "@/redux/slices/customer"
 
 const statusArray = [
-  "Manufactured",
-  "Sold",
+  "Not_Dispatched",
+  "Shipped",
   "Warranty Active",
-  "Warranty Expired",
-];
+  "Warranty Expired"
+]
 
 function ProductBoardGlobal({ scannedData }) {
-  console.log("scanned Data: ", scannedData);
-  const dispatch = useDispatch();
-  const [localLoading, setLocalLoading] = useState(null);
+  console.log("scanned Data: ", scannedData)
+  const dispatch = useDispatch()
+  const { userRole } = useSelector((state) => state.navbar)
+  const [localLoading, setLocalLoading] = useState(null)
   const [data, setData] = useState({
-    recipient_address: "",
-  });
+    recipient_address: ""
+  })
+  if (userRole == 1) Router.push(`/profile`)
 
-  const PK = process.env.CHANNEL_KEY;
-  const Pkey = `0x${PK}`;
-  const channelAddress = "0x7eff959E7D7fB6b9F3cDA78599966870929A7628";
+  // useEffect(() => {
+  //   dispatch(getProductDetailsById(scannedData))
+  // }, [])
+  // const PK = process.env.CHANNEL_KEY
+  // const Pkey = `0x${PK}`
+  // const channelAddress = "0x7eff959E7D7fB6b9F3cDA78599966870929A7628"
 
-  const { data: signer } = useSigner(Pkey);
-  const { productDetail } = useSelector((state) => state.retailer);
+  // const { data: signer } = useSigner(Pkey)
+  const { productDetail } = useSelector((state) => state.retailer)
 
-  useEffect(() => {
-    dispatch(getProductDetailsById(scannedData));
-  }, [scannedData]);
+  console.log("in v Bhavan", productDetail)
+
 
   const handleChange = (e) => {
     setData({
       ...data,
-      [e.target.name]: e.target.value,
-    });
-    console.log(data);
-  };
+      [e.target.name]: e.target.value
+    })
+    console.log(data)
+  }
   // console.log("product details in component:", productDetail)
 
   const handleClick = async () => {
+    if (data.recipient_address.length == 0) return
     try {
-      dispatch(sellProduct({ id: scannedData, add: data.recipient_address }))
+      dispatch(transferNft({ id: scannedData, to: data.recipient_address }))
         .unwrap()
         .then(() => {
-          dispatch(setSuccess("Product sold Successfully!"));
-        });
+          dispatch(
+            resellProduct({ id: scannedData, to: data.recipient_address })
+          )
+          dispatch(setSuccess("Product ownership and NFT Transferred"))
+        })
+      // dispatch(sellProduct({ id: scannedData, add: data.recipient_address }))
+      //   .unwrap()
+      //   .then(() => {
+      //     dispatch(setSuccess("Product sold Successfully!"));
+      //   });
 
-      await Push.payloads.sendNotification({
-        signer,
-        type: 3, //target
-        identityType: 2, //direct payloads
-        notification: {
-          title: "New Notification Alert",
-          body: "Message!",
-        },
-        payload: {
-          title: `Pending Notification 📌`,
-          body: `You have an activation request for ${scannedData}`, //tokenID
-          cta: "",
-          img: "",
-        },
-        recipients: data.recipient_address,
-        channel: channelAddress,
-        env: "staging",
-      });
-      dispatch(setSuccess("Notification Sent"));
-      setData({ recipient_address: "" });
+      // await Push.payloads.sendNotification({
+      //   signer,
+      //   type: 3, //target
+      //   identityType: 2, //direct payloads
+      //   notification: {
+      //     title: "New Notification Alert",
+      //     body: "Message!",
+      //   },
+      //   payload: {
+      //     title: `Pending Notification 📌`,
+      //     body: `You have an activation request for ${scannedData}`, //tokenID
+      //     cta: "",
+      //     img: "",
+      //   },
+      //   recipients: data.recipient_address,
+      //   channel: channelAddress,
+      //   env: "staging",
+      // });
+      setData({ recipient_address: "" })
     } catch (error) {
-      setData({ recipient_address: "" });
-      dispatch(setError("Unable to notify !", error.message));
+      setData({ recipient_address: "" })
+      dispatch(setError("Check your inputs", error.message))
     }
-  };
+  }
   return (
     <div className="product-detail-box">
       <div className="photo">
@@ -86,6 +100,7 @@ function ProductBoardGlobal({ scannedData }) {
           src={productDetail ? productDetail[2] : logo}
           width={100}
           height={100}
+          alt="Product Img"
         />
       </div>
       <div className="box">
@@ -130,7 +145,7 @@ function ProductBoardGlobal({ scannedData }) {
         </>
       ) : null}
     </div>
-  );
+  )
 }
 
-export default ProductBoardGlobal;
+export default ProductBoardGlobal
