bc09865748378d4895a06ad14e3c236873fe6fcc
Prasang023 <prasang023@gmail.com>
2023-03-25 16:29:06 +0530

    manufacturers function completed

--- client/components/Profile/Left.js
+++ client/components/Profile/Left.js
@@ -1,106 +1,113 @@
-import React from "react";
-import { FaRegShareSquare, FaRegHandPeace } from "react-icons/fa";
-import { ConnectWallet } from "../CustomConnect";
-import { registerManufacturer } from "@/redux/slices/manufacturer";
-import { useDispatch, useSelector } from "react-redux";
-import { checkUser } from "@/redux/navbar";
+import React from "react"
+import { FaRegShareSquare, FaRegHandPeace } from "react-icons/fa"
+import { ConnectWallet } from "../CustomConnect"
+import { clearData, registerManufacturer } from "@/redux/slices/manufacturer"
+import { useDispatch, useSelector } from "react-redux"
+import { checkUser } from "@/redux/navbar"
 import { FaRegBell, FaRegTimesCircle } from "react-icons/fa"
+import Loader from "../Loader"
 
 function Left({ setNavItem, navItem, profile, nav_items, profs }) {
-  const dispatch = useDispatch();
-  const { instances } = useSelector((state) => state.navbar);
+  const dispatch = useDispatch()
+  const { instances } = useSelector((state) => state.navbar)
+  const { loading } = useSelector((state) => state.manufacturer)
 
   const addManufacturer = () => {
     dispatch(registerManufacturer())
       .unwrap()
       .then((res) => {
-        dispatch(checkUser(instances));
-      });
-  };
+        dispatch(checkUser(instances))
+      })
+  }
 
   return (
-    <div className="dwar-profile-left">
-      <div className="profile-top-box">
-        <div
-          className="dwar-notification"
-          onClick={() => {
-            const ele = document.getElementById("dwar-notification");
-            ele.style.opacity = 1;
-            ele.style.zIndex = 1000;
-          }}
-        >
-          <FaRegBell size={16} className="icons" />
-        </div>
-        <div className="profile-top-box-round">
-          <h1>{profs[profile].a}</h1>
-        </div>
-        <div className="profile-top-box-text">
-          <p>{profs[profile].b}</p>
-        </div>
-      </div>
-      <div className="profile-bottom-box">
-        <div
-          className="profile-text-box active"
-          id={`profile-text-box-${0}`}
-          onClick={(e) => {
-            document
-              .getElementById(`profile-text-box-${navItem}`)
-              .classList.remove("active");
-            setNavItem(0);
-            document
-              .getElementById(`profile-text-box-${0}`)
-              .classList.add("active");
-          }}
-        >
-          <div className="profile-text-item-box">
-            <FaRegHandPeace size={16} className="icons" />
-            <p>{nav_items[profile][0]}</p>
+    <>
+      <div className="dwar-profile-left">
+        <div className="profile-top-box">
+          <div
+            className="dwar-notification"
+            onClick={() => {
+              const ele = document.getElementById("dwar-notification")
+              ele.style.opacity = 1
+              ele.style.zIndex = 1000
+            }}
+          >
+            <FaRegBell size={16} className="icons" />
+          </div>
+          <div className="profile-top-box-round">
+            <h1>{profs[profile].a}</h1>
+          </div>
+          <div className="profile-top-box-text">
+            <p>{profs[profile].b}</p>
           </div>
         </div>
-        {nav_items[profile].map((e, index) => {
-          if (profile == 2 && index == 3) {
-            return (
-              <div
-                className="profile-text-box remove"
-                id={`profile-text-box-${index}`}
-                onClick={addManufacturer}
-              >
-                <div className="profile-text-item-box">
-                  <p>{e}</p>
+        <div className="profile-bottom-box">
+          <div
+            className="profile-text-box active"
+            id={`profile-text-box-${0}`}
+            onClick={(e) => {
+              document
+                .getElementById(`profile-text-box-${navItem}`)
+                .classList.remove("active")
+              setNavItem(0)
+              document
+                .getElementById(`profile-text-box-${0}`)
+                .classList.add("active")
+            }}
+          >
+            <div className="profile-text-item-box">
+              <FaRegHandPeace size={16} className="icons" />
+              <p>{nav_items[profile][0]}</p>
+            </div>
+          </div>
+          {nav_items[profile].map((e, index) => {
+            if (profile == 2 && index == 3) {
+              return (
+                <div
+                  className="profile-text-box remove"
+                  id={`profile-text-box-${index}`}
+                  onClick={addManufacturer}
+                >
+                  <div className="profile-text-item-box">
+                    {loading ? <Loader height={40} width={40} /> : <p>{e}</p>}
+                  </div>
                 </div>
-              </div>
-            );
-          } else if (index > 0) {
-            return (
-              <div
-                className="profile-text-box"
-                id={`profile-text-box-${index}`}
-                onClick={(e) => {
-                  document
-                    .getElementById(`profile-text-box-${navItem}`)
-                    .classList.remove("active");
-                  setNavItem(index);
-                  document
-                    .getElementById(`profile-text-box-${index}`)
-                    .classList.add("active");
-                }}
-              >
-                <div className="profile-text-item-box">
-                  <FaRegHandPeace size={16} className="icons" />
-                  <p>{e}</p>
+              )
+            } else if (index > 0) {
+              return (
+                <div
+                  className="profile-text-box"
+                  id={`profile-text-box-${index}`}
+                  onClick={(e) => {
+                    if (index == 1 && profile == 0) {
+                      dispatch(clearData())
+                    }
+                    document
+                      .getElementById(`profile-text-box-${navItem}`)
+                      .classList.remove("active")
+                    setNavItem(index)
+                    document
+                      .getElementById(`profile-text-box-${index}`)
+                      .classList.add("active")
+                  }}
+                >
+                  <div className="profile-text-item-box">
+                    <FaRegHandPeace size={16} className="icons" />
+                    <p>{e}</p>
+                  </div>
                 </div>
-              </div>
-            );
-          }
-        })}
-        <div className="profile-text-box disconnect">
-          <div className="profile-text-item-box">
-            <ConnectWallet />
+              )
+            }
+          })}
+          <div className="profile-text-box disconnect">
+            <div className="profile-text-item-box">
+              <ConnectWallet />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    </>
+  )
 }
 
-export default Left;
+export default Left
