c1835621db3c2e388b5b747c51ea2c2786fa4bab
Averek <singhavanish2003@gmail.com>
2023-03-31 23:51:09 +0530

    theme-backend-fixes

--- client/next.config.js
+++ client/next.config.js
@@ -3,11 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     CHANNEL_PRIVATE_KEY:
-      "b96a862c2e75a28326cb308ee1326ef166c7efe99508c051fc2656b48afcacb9"
+      "b96a862c2e75a28326cb308ee1326ef166c7efe99508c051fc2656b48afcacb9",
+    BACKEND_ENDPOINT: "https://cognizance-bit-bots-backend.vercel.app",
   },
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "ipfs.io", port: "" }]
-  }
-}
+    remotePatterns: [{ protocol: "https", hostname: "ipfs.io", port: "" }],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
